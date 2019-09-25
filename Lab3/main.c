226490f4b87ab2db7bca7a015abec3607989df52
Jiajian Chen <chenj21@unlv.nevada.edu>
2019-09-25 02:11:37 -0700

    lab3

--- Lab3/main.c
+++ Lab3/main.c
@@ -5,7 +5,8 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 
-uint8_t ui8PinData=2;
+uint8_t ui8PinData = 2;
+int newdata = 4;
 int main(void)
 {
 	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
@@ -16,9 +17,28 @@ int main(void)
 	while(1)
 	{
 		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,ui8PinData);
-		SysCtlDelay(2000000);
+		SysCtlDelay(10000000);// stay on for 25ns * 10000000 = 250ms = 0.25s
 		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
-		SysCtlDelay(2000000);
-		if(ui8PinData==8){ui8PinData=2;} else {ui8PinData=ui8PinData*2;}
+		SysCtlDelay(10000000);// turn it off for 25ns * 10000000 = 250ms = 0.25s
+		if(ui8PinData == 8)
+		{
+			ui8PinData =  6;
+			//blinking RB
+		}
+		else if(ui8PinData < 8 && ui8PinData != 6)
+		{
+			ui8PinData = ui8PinData*2;
+		}//blinking R-B-G
+		else if(ui8PinData < 14)
+		{
+			ui8PinData = ui8PinData + newdata;
+			if(newdata > 2){newdata = newdata / 2;} else{newdata = 2;}
+			//blinking RG-BG-RBG
+		}
+		else
+		{
+			ui8PinData = 2;
+			newdata = 4;
+		}
 	}
 }
